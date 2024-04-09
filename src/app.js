b0364a3a5e44dd40f097f77faf4f98f1ac76899e
Andrei <andrei.vupt@gmail.com>
2024-04-09 18:09:05 -0300

    api

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-// Módulo de configuração e aplicação da webapi
-
-// Importar o pacote express (servidor)
 const express = require('express');
-// Importar o pacote dotenv, gerenciador de variáveis de ambiente
 const dotenv = require('dotenv').config();
+const taskRouter = require('./routes/taskRouter');
 
-// Instanciar o express na variável app
 const app = express();
-// Setar a porta do servidor, a parir do arquivo .env ou assumir 3005
 app.set('port', process.env.PORT || 3005);
+
+const cors = require('cors');
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', taskRouter);
+
 
 module.exports = app;
